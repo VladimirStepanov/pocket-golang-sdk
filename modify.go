8c9eb7b174e9b0a13d6dfdd79019267c52b5ff1b
Vladimir Stepanov <v.stepanov@redmadrobot.com>
2022-05-14 12:33:43 +0600

    add own type for every Action

--- modify.go
+++ modify.go
@@ -9,17 +9,23 @@ import (
 type ActionType string
 
 const (
-	ActionAddType        ActionType = "add"
-	ActionArchiveType    ActionType = "archive"
-	ActionReaddType      ActionType = "readd"
-	ActionFavoriteType   ActionType = "favorite"
-	ActionUnfavoriteType ActionType = "unfavorite"
-	ActionDeleteType     ActionType = "delete"
+	ActionAddType         ActionType = "add"
+	ActionArchiveType     ActionType = "archive"
+	ActionReaddType       ActionType = "readd"
+	ActionFavoriteType    ActionType = "favorite"
+	ActionUnfavoriteType  ActionType = "unfavorite"
+	ActionDeleteType      ActionType = "delete"
+	ActionTagsAddType     ActionType = "tags_add"
+	ActionTagsRemoveType  ActionType = "tags_remove"
+	ActionTagsReplaceType ActionType = "tags_replace"
+	ActionTagsClearType   ActionType = "tags_clear"
+	ActionTagRenameType   ActionType = "tag_rename"
+	ActionTagDeleteType   ActionType = "tag_delete"
 )
 
 type Actions []interface{}
 
-type Action struct {
+type action struct {
 	Action ActionType `json:"action"`
 	ItemID int64      `json:"item_id"`
 	Time   int64      `json:"time,omitempty"`
@@ -33,6 +39,40 @@ type ActionAdd struct {
 	Title  string     `json:"title,omitempty"`
 	Url    string     `json:"url,omitempty"` // MUST BE ENCODED
 }
+
+type tagsAction struct {
+	Action ActionType `json:"action"`
+	ItemID string     `json:"item_id"`
+	Tags   string     `json:"tags"`
+	Time   int64      `json:"time,omitempty"`
+}
+
+type ActionTagRename struct {
+	Action ActionType `json:"action"`
+	ItemID string     `json:"item_id"`
+	OldTag string     `json:"old_tag"`
+	NewTag string     `json:"new_tag"`
+	Time   int64      `json:"time,omitempty"`
+}
+
+type ActionTagDelete struct {
+	Action ActionType `json:"action"`
+	ItemID string     `json:"item_id"`
+	Tag    string     `json:"tag"`
+	Time   int64      `json:"time,omitempty"`
+}
+
+type (
+	ActionArchive     action
+	ActionReadd       action
+	ActionFavorite    action
+	ActionUnfavorite  action
+	ActionDelete      action
+	ActionTagsAdd     tagsAction
+	ActionTagsRemove  tagsAction
+	ActionTagsReplace tagsAction
+	ActionTagsClear   action
+)
 
 type modifyRequest struct {
 	Actions     Actions `json:"actions"`
